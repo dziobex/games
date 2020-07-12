8337cc41ae2a740b26762db232b8da138deea9b9
Dziobax1 <58388062+Dziobax1@users.noreply.github.com>
2020-07-12 15:08:15 +0200

    Create Program.cs

---
+++ 5. Hangman/Program.cs
@@ -0,0 +1,19 @@
+using System;
+using System.Windows.Forms;
+
+namespace Hangman
+{
+    static class Program
+    {
+        /// <summary>
+        /// Główny punkt wejścia dla aplikacji.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
+        }
+    }
+}
